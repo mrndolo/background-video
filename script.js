65a6f049444d33ee99adae23c4ff42a968c539bc
mrndolo <callistomutunga@gmail.com>
2024-08-18 19:32:25 -0400

    Selected Ui components

---
+++ script.js
@@ -0,0 +1,4 @@
+const btn = document.querySelector(".btn");
+const video = document.querySelector(".background-video");
+const fa = document.querySelector(".fa");
+const preloader = document.querySelector(".preloader");
